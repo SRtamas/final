a69da7288825632ba507724c9edc035b216d4282
SRtamas <2201222@s.asojuku.ac.jp>
2024-01-11 10:05:08 +0900

    いｎ

---
+++ src/cook_Select.php
@@ -0,0 +1,48 @@
+<?php
+    const SERVER = 'mysql220.phy.lolipop.lan';
+    const DBNAME = 'LAA1516820-final';
+    const USER = 'LAA1516820';
+    const PASS = 'Pass0830';
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>料理管理システム</title>                                 
+</head>
+<body>
+    <div class="list_field">
+    <h1>料理一覧</h1>
+    <a href="index.php">
+        <button type="button" class="re">商品登録</button>
+    </a>
+    <hr>
+    <table align="center" border="1">
+        <tr>
+            <th>料理番号</th>
+            <th>料理名</th>
+            <th>ジャンル</th>
+        </tr>
+<?php
+$pdo = new PDO($connect, USER, PASS);
+$sql = $pdo->query('SELECT * FROM cook');
+foreach($sql as $row){
+
+    echo '<tr>';
+    echo '<td>', $row['cook_id'], '</td>';
+    echo '<td>', $row['cook_mei'], '</td>';
+    echo '<td>', $row['cook_genre'], '</td>';
+    echo '<td>';
+    echo '<div class="delete"><a href="cook-Delet.php?id=', $row['cook_id'],'"><button type="button">削除</button></a></div>';
+    echo '<div class="update"><a href="cook-update.php?id=', $row['cook_id'],'"><button type="button">更新</button></a></div>';
+    echo '</td>';
+    echo '</tr>';
+    echo "\n";
+}
+
+?>
+</table>
+</div>
+</body>
+</html>
--- src/index.php
+++ src/index.php
@@ -1,48 +1,15 @@
-
-<?php
-    const SERVER = 'mysql220.phy.lolipop.lan';
-    const DBNAME = 'LAA1516820-final';
-    const USER = 'LAA1516820';
-    const PASS = 'Pass0830';
-    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
-?>
 <!DOCTYPE html>
-<html lang="ja">
+<html>
 <head>
     <meta charset="UTF-8">
-    <title>料理管理システム</title>                                 
+    <title>music-menu(CRUD)</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-    <div class="list_field">
-    <h1>商品一覧画面</h1>
-    <a href="cook_Insert.php">
-        <button type="button" class="re">商品登録</button>
-    </a>
-    <table align="center" border="1">
-        <tr>
-            <th>料理番号</th>
-            <th>料理名</th>
-            <th>ジャンル</th>
-        </tr>
-<?php
-$pdo = new PDO($connect, USER, PASS);
-$sql = $pdo->query('SELECT * FROM cook');
-foreach($sql as $row){
-
-    echo '<tr>';
-    echo '<td>', $row['cook_id'], '</td>';
-    echo '<td>', $row['cook_mei'], '</td>';
-    echo '<td>', $row['cook_genre'], '</td>';
-    echo '<td>';
-    echo '<div class="delete"><a href="cook-Delet.php?id=', $row['cook_id'],'"><button type="button">削除</button></a></div>';
-    echo '<div class="update"><a href="cook-update.php?id=', $row['cook_id'],'"><button type="button">更新</button></a></div>';
-    echo '</td>';
-    echo '</tr>';
-    echo "\n";
-}
-
-?>
-</table>
-</div>
+<h2>料理管理メニュー</h2>
+    <a href="cook-Select.php" button type="button">一覧</button></a>
+    <a href="cook-Delet.php" button type="button">削除</button></a>
+    <a href="cook-Insert.php" button type="button">登録</button></a>
+    <a href="cook-Updata.php" button type="button">更新</button></a>
 </body>
 </html>
