a1be8cb47c10fc2e803ad604307b0b24a836f8c9
SRtamas <2201222@s.asojuku.ac.jp>
2024-01-11 10:07:09 +0900

    in

--- src/index.php
+++ src/index.php
@@ -7,9 +7,9 @@
 </head>
 <body>
 <h2>料理管理メニュー</h2>
-    <a href="cook-Select.php" button type="button">一覧</button></a>
-    <a href="cook-Delet.php" button type="button">削除</button></a>
-    <a href="cook-Insert.php" button type="button">登録</button></a>
-    <a href="cook-Updata.php" button type="button">更新</button></a>
+    <a href="cook-Select.php"><button type="button">一覧</button></a>
+    <a href="cook-Delet.php"><button type="button">削除</button></a>
+    <a href="cook-Insert.php"><button type="button">登録</button></a>
+    <a href="cook-Updata.php"><button type="button">更新</button></a>
 </body>
 </html>
