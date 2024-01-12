0440b1b53aeb9922b8b9baeb81dffcb193832ee7
SRtamas <2201222@s.asojuku.ac.jp>
2024-01-12 11:49:50 +0900

    a

--- src/Delete-input.php
+++ src/Delete-input.php
@@ -18,7 +18,6 @@
     <hr>
     <table>
         <tr>
-            <th>料理番号</th>
             <th>料理名</th>
             <th>ジャンル</th>
         </tr>
@@ -28,7 +27,7 @@ $sql = $pdo->query('SELECT * FROM cook');
 foreach($sql as $row){
 
     echo '<tr>';
-    echo '<td>', $row['cook_id'], '</td>';
+    echo '<td><input type = "hidden" name = "', $row['cook_id'] ,'"</td>';
     echo '<td>', $row['cook_mei'], '</td>';
     echo '<td>', $row['cook_genre'], '</td>';
     echo '<td><div class="delete"><a href="Delete-output.php?id=', $row['cook_id'],'"><button type="button">削除</button></a></td></div>';
