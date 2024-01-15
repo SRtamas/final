ddfbf82cda2d2f1e9d690f758d1141f5ef076baf
SRtamas <2201222@s.asojuku.ac.jp>
2024-01-15 10:16:41 +0900

    a

--- src/Update-output.php
+++ src/Update-output.php
@@ -14,14 +14,18 @@
 <body>
 <?php
     $pdo=new PDO($connect, USER, PASS);
+    $sql = $pdo->prepare('select * from cook where cook_id = ?');
+    $sql->execute([$_GET['id']]);
+    foreach($sql as $row){
+
     echo '<form action="Upadate-result.php">';
-    echo '<input type="hidden" name="cook_id" value="',$_POST['cook_id'],'required">';
+    echo '<input type="hidden" name="cook_id" value="',$row['cook_id'],'required">';
     echo '<table>';
     echo '<tr><th>料理名</th><th>ジャンル</th></tr>';
     echo '<tr>';
     echo '<div class="td1">';
-    echo '<td><input type="text" name="', $_POST['cook_mei'] ,'" value="',$_POST['cook_mei'],'"></td>';
-    echo '<td><select name="', $_POST['cook_genre'],'">';
+    echo '<td><input type="text" name="', $row['cook_mei'] ,'" value="',$row['cook_mei'],'"></td>';
+    echo '<td><select name="', $row['cook_genre'],'">';
     echo '<option value="和食">和食</option>';
     echo '<option value="中華">中華</option>';
     echo '<option value="洋食">洋食</option>';
@@ -30,6 +34,7 @@
     echo '</div>';
     echo '<input type = "submit" value = "更新">';
     echo '</form>'
+    }
 ?>
 </body>
 </html>
