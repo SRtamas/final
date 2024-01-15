862097871afc8083ec67513d23bdee69fc0ac1d7
SRtamas <2201222@s.asojuku.ac.jp>
2024-01-15 10:31:05 +0900

    a

--- src/Update-result.php
+++ src/Update-result.php
@@ -17,7 +17,7 @@
 <?php
     $pdo=new PDO($connect, USER, PASS);
     $sql=$pdo->prepare('update cook set cook_mei=?,cook_genre=? where cook_id=?');
-    if($sql->execute([htmlspecialchars($_POST['cook_mei']),$_POST['cook_genre'],$_GET['id']])){
+    if($sql->execute([$_POST['cook_mei'],$_POST['cook_genre'],$_GET['id']])){
                 echo '<h1>更新に成功しました。</h1>';
             }else{
                 echo '<h1>更新に失敗しました。</h1>';
